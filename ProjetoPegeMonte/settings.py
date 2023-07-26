d2137b2218fee31e5c188ff42db0bc1d7d45df6b
BrunoLK021 <brunoalencarvieira136@gmail.com>
2023-07-26 10:21:38 -0300

    static / DEBUG

--- ProjetoPegeMonte/settings.py
+++ ProjetoPegeMonte/settings.py
@@ -139,14 +139,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-if DEBUG == False:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+if DEBUG == True:
+    STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+    STATICFILES_DIRS = [
+        BASE_DIR / "static",
+    ]
+
+if DEBUG == False:
+    STATIC_URL = 'static/'
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = "media/"
 
