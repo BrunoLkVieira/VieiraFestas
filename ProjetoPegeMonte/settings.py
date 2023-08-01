445b64f309d9f3ad3e830af438040aba3350ee31
BrunoLK021 <brunoalencarvieira136@gmail.com>
2023-07-31 21:48:48 -0300

    media file 3

--- ProjetoPegeMonte/settings.py
+++ ProjetoPegeMonte/settings.py
@@ -149,8 +149,8 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = "media/"
 
-MEDIA_ROOT = BASE_DIR / "media"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / "mediafiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- ProjetoPegeMonte/urls.py
+++ ProjetoPegeMonte/urls.py
@@ -20,7 +20,6 @@ from django.conf.urls.static import static
 from django.views.static import serve
 
 static_urlpatterns = [
-    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
     re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
 ]
 
@@ -31,7 +30,5 @@ urlpatterns = [
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-if settings.DEBUG:
-    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
